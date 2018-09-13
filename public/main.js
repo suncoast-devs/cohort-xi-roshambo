489b6b4acbe47b14777db14acad8b4ce8f69284b
Allan <allan@Allans-MBP.localdomain>
2018-09-13 11:26:03 -0400

    refactoring

--- public/main.js
+++ public/main.js
@@ -1,6 +1,52 @@
 let p1choice = "";
 let p2choice = "";
 
+const detectWinner = () => {
+  //if players 1 choice is not a blank string and players 2 choice is not a blank string
+  if (p1choice !== "" && p2choice !== "") {
+    let footer = document.querySelector("footer");
+    //   if player one chose rock and player two chose paper then the winner is player two
+    if (p1choice === "rock" && p2choice === "paper") {
+      footer.textContent = "player 2 wins";
+    }
+    //   if player one chose rock and player two chose rock then there is a tie
+    if (p1choice === "rock" && p2choice === "rock") {
+      footer.textContent = "tie";
+    }
+    //   if player one chose rock and player two chose scissors then the winner is player one
+    if (
+      (p1choice === "rock" && p2choice === "scissors") ||
+      (p1choice === "paper" && p2choice === "rock")
+    ) {
+      footer.textContent = "player 1 wins";
+    }
+    //   if player one chose paper and player two chose rock then the winner is player one
+    if (p1choice === "paper" && p2choice === "rock") {
+      footer.textContent = "player 1 wins";
+    }
+    //   if player one chose paper and player two chose paper then there is a tie
+    if (p1choice === "paper" && p2choice === "paper") {
+      footer.textContent = "tie";
+    }
+    //   if player one chose paper and player two chose scissors then the winner is player two
+    if (p1choice === "paper" && p2choice === "scissors") {
+      footer.textContent = "player 2 wins";
+    }
+    //   if player one chose scissors and player two chose rock then the winner is player two
+    if (p1choice === "scissors" && p2choice === "rock") {
+      footer.textContent = "player 2 wins";
+    }
+    //   if player one chose scissors and player two chose paper then the winner is player one
+    if (p1choice === "scissors" && p2choice === "paper") {
+      footer.textContent = "player 1 wins";
+    }
+    //   if player one chose scissors and player two chose scissors then there is a tie
+    if (p1choice === "scissors" && p2choice === "scissors") {
+      footer.textContent = "tie";
+    }
+  }
+};
+
 const clickedOnPlayerOne = event => {
   document.querySelector(".player-1").classList.add("choice-made");
 
@@ -24,134 +70,20 @@ const clickedOnPlayerOne = event => {
   // AND    &&
   // OR     ||
 
-  //if players 1 choice is not a blank string and players 2 choice is not a blank string
-  if (p1choice !== "" && p2choice !== "") {
-    //   if player one chose rock and player two chose paper then the winner is player two
-    if (p1choice === "rock" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose rock and player two chose rock then there is a tie
-    if (p1choice === "rock" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose rock and player two chose scissors then the winner is player one
-    if (p1choice === "rock" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose rock then the winner is player one
-    if (p1choice === "paper" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose paper then there is a tie
-    if (p1choice === "paper" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose paper and player two chose scissors then the winner is player two
-    if (p1choice === "paper" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose rock then the winner is player two
-    if (p1choice === "scissors" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose paper then the winner is player one
-    if (p1choice === "scissors" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose scissors and player two chose scissors then there is a tie
-    if (p1choice === "scissors" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "tie";
-    }
-  }
+  detectWinner();
 };
 let clickedOnPlayerOnePaper = event => {
   document.querySelector(".player-1").classList.add("choice-made");
 
   p1choice = "paper";
-  //if players 1 choice is not a blank string and players 2 choice is not a blank string
-  if (p1choice !== "" && p2choice !== "") {
-    //   if player one chose rock and player two chose paper then the winner is player two
-    if (p1choice === "rock" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose rock and player two chose rock then there is a tie
-    if (p1choice === "rock" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose rock and player two chose scissors then the winner is player one
-    if (p1choice === "rock" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose rock then the winner is player one
-    if (p1choice === "paper" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose paper then there is a tie
-    if (p1choice === "paper" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose paper and player two chose scissors then the winner is player two
-    if (p1choice === "paper" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose rock then the winner is player two
-    if (p1choice === "scissors" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose paper then the winner is player one
-    if (p1choice === "scissors" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose scissors and player two chose scissors then there is a tie
-    if (p1choice === "scissors" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "tie";
-    }
-  }
+  detectWinner();
 };
 
 let clickedOnPlayerOneScissors = event => {
   document.querySelector(".player-1").classList.add("choice-made");
 
   p1choice = "scissors";
-  //if players 1 choice is not a blank string and players 2 choice is not a blank string
-  if (p1choice !== "" && p2choice !== "") {
-    //   if player one chose rock and player two chose paper then the winner is player two
-    if (p1choice === "rock" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose rock and player two chose rock then there is a tie
-    if (p1choice === "rock" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose rock and player two chose scissors then the winner is player one
-    if (p1choice === "rock" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose rock then the winner is player one
-    if (p1choice === "paper" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose paper then there is a tie
-    if (p1choice === "paper" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose paper and player two chose scissors then the winner is player two
-    if (p1choice === "paper" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose rock then the winner is player two
-    if (p1choice === "scissors" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose paper then the winner is player one
-    if (p1choice === "scissors" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose scissors and player two chose scissors then there is a tie
-    if (p1choice === "scissors" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "tie";
-    }
-  }
+  detectWinner();
 };
 
 const clickedOnPlayerTwo = event => {
@@ -159,157 +91,20 @@ const clickedOnPlayerTwo = event => {
 
   // Somehow remember that player 1 chose "rock"
   p2choice = "rock";
-  //if players 1 choice is not a blank string and players 2 choice is not a blank string
-  if (p1choice !== "" && p2choice !== "") {
-    //   if player one chose rock and player two chose paper then the winner is player two
-    if (p1choice === "rock" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose rock and player two chose rock then there is a tie
-    if (p1choice === "rock" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose rock and player two chose scissors then the winner is player one
-    if (p1choice === "rock" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose rock then the winner is player one
-    if (p1choice === "paper" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose paper then there is a tie
-    if (p1choice === "paper" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose paper and player two chose scissors then the winner is player two
-    if (p1choice === "paper" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose rock then the winner is player two
-    if (p1choice === "scissors" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose paper then the winner is player one
-    if (p1choice === "scissors" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose scissors and player two chose scissors then there is a tie
-    if (p1choice === "scissors" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "tie";
-    }
-  }
-  // Check if both players have made a choice
-
-  // comparison operators
-  // ===    equal to
-  // !==    not equal to
-  // >      great than
-  // <      less than
-  // >=     greater than or equal to
-  // <=     less than or equal to
-
-  // AND    &&
-  // OR     ||
-
-  // if both players have made a choice
-  //   if player one chose rock and player two chose paper then the winner is player two
-  //   if player one chose rock and player two chose rock then there is a tie
-  //   if player one chose rock and player two chose scissors then the winner is player one
-  //   if player one chose paper and player two chose rock then the winner is player one
-  //   if player one chose paper and player two chose paper then there is a tie
-  //   if player one chose paper and player two chose scissors then the winner is player two
-  //   if player one chose scissors and player two chose rock then the winner is player two
-  //   if player one chose scissors and player two chose paper then the winner is player one
-  //   if player one chose scissors and player two chose scissors then there is a tie
+  detectWinner();
 };
 let clickedOnPlayerTwoPaper = event => {
   document.querySelector(".player-2").classList.add("choice-made");
 
   p2choice = "paper";
-  //if players 1 choice is not a blank string and players 2 choice is not a blank string
-  if (p1choice !== "" && p2choice !== "") {
-    //   if player one chose rock and player two chose paper then the winner is player two
-    if (p1choice === "rock" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose rock and player two chose rock then there is a tie
-    if (p1choice === "rock" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose rock and player two chose scissors then the winner is player one
-    if (p1choice === "rock" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose rock then the winner is player one
-    if (p1choice === "paper" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose paper then there is a tie
-    if (p1choice === "paper" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose paper and player two chose scissors then the winner is player two
-    if (p1choice === "paper" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose rock then the winner is player two
-    if (p1choice === "scissors" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose paper then the winner is player one
-    if (p1choice === "scissors" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose scissors and player two chose scissors then there is a tie
-    if (p1choice === "scissors" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "tie";
-    }
-  }
+  detectWinner();
 };
 
 let clickedOnPlayerTwoScissors = event => {
   document.querySelector(".player-2").classList.add("choice-made");
 
   p2choice = "scissors";
-  //if players 1 choice is not a blank string and players 2 choice is not a blank string
-  if (p1choice !== "" && p2choice !== "") {
-    //   if player one chose rock and player two chose paper then the winner is player two
-    if (p1choice === "rock" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose rock and player two chose rock then there is a tie
-    if (p1choice === "rock" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose rock and player two chose scissors then the winner is player one
-    if (p1choice === "rock" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose rock then the winner is player one
-    if (p1choice === "paper" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose paper and player two chose paper then there is a tie
-    if (p1choice === "paper" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "tie";
-    }
-    //   if player one chose paper and player two chose scissors then the winner is player two
-    if (p1choice === "paper" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose rock then the winner is player two
-    if (p1choice === "scissors" && p2choice === "rock") {
-      document.querySelector("footer").textContent = "player 2 wins";
-    }
-    //   if player one chose scissors and player two chose paper then the winner is player one
-    if (p1choice === "scissors" && p2choice === "paper") {
-      document.querySelector("footer").textContent = "player 1 wins";
-    }
-    //   if player one chose scissors and player two chose scissors then there is a tie
-    if (p1choice === "scissors" && p2choice === "scissors") {
-      document.querySelector("footer").textContent = "tie";
-    }
-  }
+  detectWinner();
 };
 
 const main = () => {
